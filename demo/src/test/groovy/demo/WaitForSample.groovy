8e0139395d224c30da7f215d1786dceb34700a25
Markus Schlichting <markus.schlichting@karakun.com>
2019-08-26 20:58:30 +0200

    extend waitfor sample

--- demo/src/test/groovy/demo/WaitForSample.groovy
+++ demo/src/test/groovy/demo/WaitForSample.groovy
@@ -3,16 +3,22 @@ package demo
 import geb.Browser
 
 Browser.drive {
-    go "https://mynethome.de/animation.html"
+    go "https://www.mynethome.de/animation.html"
 
     assert title == "Animation For Waitfor Example"
+
+    assert "Show us the modal!" == $("div#app h3").text()
 
     $("div#app button").click()
     waitFor { !$("div.modal").hasClass("fade-enter-active") }
 
+
+    assert $("div.modal").displayed && !$("div.modal").hasClass("fade-enter-active")
+    assert "Here I am!" == $("div.modal h2").text()
+
     $("div.modal button").click()
     waitFor { !$("div.modal").hasClass("fade-leave-active") }
 
+    assert $("div.modal").displayed == false
 }
-
 
